57f7964df757bddf4a30ee96f9b54619a2a6e65b
João Paulo Marianito Fonseca <jpm_fonseca@hotmail.com>
2024-10-16 09:58:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  
+
+  var member = {
+    name: "John",
+    age: 30,
+    skills: ["JavaScript", "React", "Node"]
+  };
+
+  return member;                                                           
+  
+
+}
